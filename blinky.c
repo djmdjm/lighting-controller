bac2b8531f7c3d797226f7fa8cdb296d06f002ba
Damien Miller <djm@mindrot.org>
2013-10-30 21:06:11 +1100

    update lamp-test app

--- blinky.c
+++ blinky.c
@@ -17,7 +17,10 @@
 /* Lamp/LCD test application */
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #include <util/delay.h>
+#include <stddef.h>
+#include <stdint.h>
 
 #include "rgbled.h"
 #include "demux.h"
@@ -25,30 +28,63 @@
 #include "num_format.h"
 #include "spi.h"
 #include "ad56x8.h"
+#include "encoder.h"
+#include "event.h"
+
+/* LUT for mux wiring */
+static const int mux_order[16] = {
+	7, 6, 5, 4, 3, 1, 2, 0, 10, 11, 8, 9, 14, 15, 12, 13
+};
 
 int
 main(void)
 {
-	int i;
-	uint16_t j;
+	int i, x, e;
+	uint8_t ev_type;
+	uint16_t ev_value;
+	char buf[16];
 
 	demux_setup();
 	rgbled_setup();
 	lcd_setup();
 	spi_setup();
-	ad56x8_setup(1);
+	//ad56x8_setup(1);
+	encoder_setup();
+	sei();
 
-	lcd_moveto(0, 0);
-	lcd_string("Hello Hugo");
+	for (x = i = 0;; i++) {
+		//ad56x8_write_update(-1, i);
+		rgbled_n(i);
+		demux_set_line(mux_order[i % 16]);
+		lcd_moveto(0, 0);
+		lcd_string(rjustify(ntod(i), buf, 7));
+		lcd_string(" Julius & Hugo");
 
-	for (i = 0;; i++) {
-		ad56x8_write_update(-1, i);
-		rgbled_n(i & 0xf);
-		demux_set_line(i % 16);
 		lcd_moveto(0, 1);
-		lcd_string(ntod(i));
-		lcd_string("       ");
-		for (j = 0; j < 512; j++)
-			ad56x8_write_update(-1, j * 256);
+		lcd_string(rjustify(ntod(encoder_value()), buf, 7));
+
+		e = event_dequeue(&ev_type, &ev_value);
+		while (event_dequeue(NULL, NULL))
+			;
+		if (e != 0 || x-- == 0) {
+			if (e != 0) {
+				lcd_moveto(7, 1);
+				lcd_string(rjustify(ntoh(ev_type, 0), buf, 3));
+				lcd_string(" ");
+				lcd_string(rjustify(ntoh(ev_value, 0), buf, 5));
+				x = 16;
+			} else {
+				lcd_moveto(7, 1);
+				lcd_string("       ");
+			}
+		}
+
+		lcd_moveto(15, 1);
+		lcd_string(rjustify(ntoh(event_nqueued(), 0), buf, 3));
+
+		lcd_moveto(18, 1);
+		lcd_string(rjustify(ntoh(event_maxqueued(), 0), buf, 3));
+
+		_delay_ms((60 * 250)/140);
 	}
 }
